b6b7367380b788359435c803f06dec4398e7622e
Russell Tan <sell_nat@yahoo.com>
2014-02-21 12:14:08 -0800

    Blank activity

---
+++ Life-App/src/com/example/life_app/LifeApp.java
@@ -0,0 +1,22 @@
+package com.example.life_app;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class LifeApp extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_life_app);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.life_app, menu);
+		return true;
+	}
+
+}
